97281a852ab55ebc01b0f001d01b8076a0a88a74
Nikita Travkin <nikita@trvn.ru>
2021-09-06 20:39:28 +0500

    custom/halflife-launcher: New package

---
+++ custom/halflife-launcher/hl-launcher.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+
+export LD_LIBRARY_PATH=/usr/lib/xash3d
+export XASH3D_EXTRAS_PAK1=/usr/share/xash3d/valve/extras.pak
+export XASH3D_BASEDIR=$HOME/.xash3d 
+
+GAME_NAME="valve"
+REFDIR="/usr/lib/xash3d"
+
+if [ ! -d $HOME/.xash3d/$GAME_NAME ]; then
+	zenity --error --text="No game data found!\nCopy \"$GAME_NAME\" dir from Steam Half-Life distribution into \"~/.xash3d\""
+	exit 1
+fi
+
+refs=""
+for r in $REFDIR/libref*
+do
+	echo $(basename $r)
+	refs="$refs $(basename $r)"
+done
+
+chosen_ref=$(zenity --list --text="Select graphic backend" --column=ref $refs | sed 's/libref_\(.*\)\.so/\1/')
+lib_arch=$(basename /usr/lib/hlsdk/$GAME_NAME/dlls/* | sed 's/hl_\(.*\)\.so/\1/')
+
+/usr/lib/xash3d/xash3d \
+	-dll /usr/lib/hlsdk/$GAME_NAME/dlls/hl_$lib_arch.so \
+	-clientlib /usr/lib/hlsdk/$GAME_NAME/cl_dlls/client_$lib_arch.so \
+	-ref $chosen_ref \
+	$@
+
